4ff76653bf34f404ce48521787014ef95eec04de
Hanish Abdulla <67541926+cysliac@users.noreply.github.com>
2024-07-02 11:03:44 +0300

    finalard

--- finalard1.py
+++ finalard1.py
@@ -1,6 +1,13 @@
-import serial
-import serial.tools.list_ports
+import os
+import cv2
 import threading
+import serial.tools.list_ports
+import serial
+from google.cloud import vision
+from google.cloud.vision_v1 import types
+
+# Set the environment variable for Google Cloud credentials
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"Google Cloud API Keys.json"
 
 def list_serial_ports():
     ports = serial.tools.list_ports.comports()
@@ -43,6 +50,9 @@ def main():
     try:
         ser = serial.Serial(selected_port, 9600, timeout=1)
         print(f"Connected to {selected_port}")
+
+        # Initialize Google Cloud Vision client
+        client = vision.ImageAnnotatorClient()
 
         # Start a thread to process image and send data to serial port
         thread = threading.Thread(target=process_image, args=(ser,))
